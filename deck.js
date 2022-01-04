4076e9bd023836c868b9c4c25639061bc1c94c74
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-01-03 19:36:21 -0700

    initial commit

---
+++ deck.js
@@ -0,0 +1,80 @@
+//creat a class to hold the entire deck and it's functions. 
+//OOP 
+const SUITS = ['♥','♦','♠', '♣'] //static global variable so it can be in all caps
+const VALUES = ['A','2', '3', '4','5','6','7','8', '9','10','J','Q','K']
+const masterDeck = []
+const mixedDeck = []
+console.log(masterDeck)
+
+
+
+//use two for loops
+function createDeck (){
+    masterDeck =[] 
+    SUITS.flatMap(
+        (suit) => { 
+            VALUES.map(
+                (value) => {
+                    let card = value + suit
+                    // console.log(card)
+                    masterDeck.push(card)
+                
+                }
+            )
+    
+        }
+    )
+}
+
+
+
+
+
+
+// console.log(newDeck)
+
+ ///copy the deck 
+
+function shuffleDeck(){
+    let shuffleDeck = [...masterDeck] //create a new play deck to shuffle 
+    //logic to shuffle the deck//
+    return shuffleDeck 
+
+}
+
+function startGame(){    ///this will go at bottom 
+createDeck()
+mixedDeck = shuffleDeck() 
+console.log(mixedDeck) 
+
+}
+
+
+
+
+class CardDeck {
+        constructor(cards){
+          this.cards = cards //   create an object to hold all the instances and functionalities of the deck 
+        }
+}
+
+class Card {                        
+    constructor(suit, value){    //create an object to hold all instances of a single card 
+        this.suit= suit
+        this.value = value 
+    }
+}
+
+//create a function that loops through all the suits and the values and combines them 
+//into a deck array.. 
+
+
+
+
+
+// The flatMap() method returns a new array formed by applying a given callback 
+// function to each element of the array, and then flattening the result by 
+// one level. It is identical to a map() followed by a flat() of depth 1, but slightly more efficient 
+// than calling those two methods separately.
+//it will run the map method on an array and then the flat method. combo of them both 
+
---
+++ wireframe.js
@@ -0,0 +1,44 @@
+/*-------- //WireFrame:// --------/* 
+HTML/CSS
+//Title of the page "Let's Play War "
+// an area that says whether the player won or lost the turn 
+// using the same area to also display when the entire game is won
+// an area displaying the two decks of cards the player and the computer will click from 
+// two "decks" displayed for each player. One with the cards face down, and the other, with the card they click face up 
+// will need to randomly generate the card they click for their turn for their turn 
+//Also need to render the deck of cards 
+//make each deck be a different color on the back, so you can tell each player apart (use gradients)
+// use css Flex to render the cards 
+
+
+/*-------- //Setting up files for the game :// --------/* 
+
+//Create a JS file for the deck that is seperate from the main logic script file? I read about this but not sure how to do it 
+// so you can use the cards for any game, not just this one 
+
+
+/*-------- //Pseudo Code:// --------/* 
+
+//Will need to render the card deck/create the cards 
+//define constant and state variables in a global scope 
+    //const suits = ["hearts", "spades","daimonds", "clubs"]
+    //const cardValues =[all the card values ]
+    //let playerOne =
+    //let computer =
+    //let scorecore = 
+    // create an array for the suits and the cardValues and combine them(use array.concat?)
+//write  a function that shuffles the deck and turns over a card when the deck is clicked by player 
+//write a function that takes one card awway from the deck when the deck is clicked by that player . 
+    //each player starts with 26 cards, each time they click, the deck is reduced by one
+//write a function that checks which card is higher aka who won the round 
+//write a function for displaying  which player won for that round 
+    //if playerOne's card is higher than computer, playerOne wins that round 
+    //if playeOne's card is lower thancomputer, playerOne lost that round 
+    //if computer card is higher than playerOne , computer wins that round 
+    //if computer card is lower than playerOne, computer lost that round 
+//write a function to declare who won the entire game when one player has all the cards 
+//write a function for when both players draw the same card, the round is null and they put the cards back 
+//write a function for when one player has all the cards they win the game 
+*/
+
+
